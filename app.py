b580610cc1e073bd636b7847d6c99c5c85753628
Eduardo Wundervald <eduardowundervald1308@gmail.com>
2025-07-28 21:08:00 -0300

    python

---
+++ app.py
@@ -0,0 +1,8 @@
+print('Sabor Express\n')
+
+print('1. Cadastrar restaurantes')
+print('2. Lista restaurantes')
+print('3. Ativar restaurante') 
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
